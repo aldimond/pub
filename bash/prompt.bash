a6f5ee526920c570e81c8c0d8ddb4a6c91912b64
Al Dimond <ald@extrahop.com>
2023-09-05 11:10:56 -0700

    add timing to bash prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -3,6 +3,18 @@
 source `dirname ${BASH_SOURCE}`/colors.bash
 
 
+# Start timing at the first command after the last prompt.
+trap 'test -v _awd_start || _awd_start=${EPOCHREALTIME/./}' DEBUG
+
+# Capture return code, stop timing.
+PROMPT_COMMAND=( \
+    # Capture return code
+    '_awd_status="$?"' \
+    # Stop timing
+    '_awd_t=$(( ${EPOCHREALTIME/./} - $_awd_start ))' \
+    'unset _awd_start' )
+
+
 # Defaults
 c1="${1:-${PBOLD}(^_^)}"
 c2="${2:-${PBOLD}${PROMPT_FG_RED}¯\(°_o)/¯}"
@@ -13,9 +25,14 @@ cerr="${3:-(╯°Д°)╯ ┻━┻}"
 # NB: backtick is evaluated *after* backslash evals, so the color string can't
 # be part of it (you could `echo -e` but you wouldn't get the non-printing
 # character delimeters)
-PS1="${PRESET}${PBOLD}${PROMPT_FG_RED}"'`a="$?" ; if [ "${a}" -ne 0 ] ; then echo "${cerr} (${a}) " ; fi`'
+PS1="${PRESET}${PBOLD}${PROMPT_FG_RED}"'`if [ "${_awd_status}" -ne 0 ] ; then echo "${cerr} (${_awd_status}) " ; fi ; `'
+
+# (Optionally) last command's run time.
+PS1+="${PROMPT_FG_BLACK}"'`if [ ${#_awd_t} -gt 6 ] ; then echo "【┘】${_awd_t:0:-6}s " ; fi ; `'
+
 # Datetime
 PS1+="${PROMPT_FG_GREEN}[ \d \t ] "
+
 # User, host
 PS1+="${PRESET}${PROMPT_FG_BLUE}\u@\h "
 # Screen/Tmux status from screen/tmux vars.
@@ -25,8 +42,10 @@ elif [ -n "${TMUX}" ] ; then
     # NB: This backtick runs at source time, not PS1 eval time
     PS1+="${PRESET}(tmux:`tmux display-message -t ${TMUX_PANE} -p '#S:#I.#P'`) "
 fi
+
 # Working dir, newline
 PS1+="${PBOLD}${PROMPT_FG_GREEN}\w\n"
+
 # Finally, our hero
 PS1+="${PRESET}${c1}${PRESET} "
 
