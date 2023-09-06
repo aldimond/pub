c76abcfcf793ac3319ef7894b26dd695aa9c02e1
Al Dimond <ald@extrahop.com>
2023-09-06 11:34:57 -0700

    prompt: format times

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -12,13 +12,29 @@ cerr="${3:-(╯°Д°)╯ ┻━┻}"
 # Start timing at the first command after the last prompt.
 trap 'test -v _awd_start || _awd_start=${EPOCHREALTIME/./}' DEBUG
 
+# $1: number of seconds
+# $2: unit
+_awd_ft_append() {
+    if [ "${_awd_t}" -ge "$1" ] ; then
+        _awd_ft+=$(( _awd_t / $1 ))$2
+        _awd_t=$(( _awd_t % $1 ))
+    fi
+}
+
+declare -i _awd_t
+
 # Use PROMPT_COMMAND to set up some stuff used in the prompt.
 PROMPT_COMMAND=(
     # Capture return code, turning 0 into nothing
     '_awd_status="${?#0}"'
     # Stop timing; squash it down if <1s
-    '_awd_t=$(( ${EPOCHREALTIME/./} - $_awd_start ))'
-    'if [ ${#_awd_t} -gt 6 ] ; then _awd_t=${_awd_t:0:-6} ; else _awd_t= ; fi'
+    '_awd_t=$(( ( ${EPOCHREALTIME/./} - $_awd_start ) / 1000000 ))'
+    '_awd_ft='
+    '_awd_ft_append 604800 w'
+    '_awd_ft_append 86400 d'
+    '_awd_ft_append 3600 h'
+    '_awd_ft_append 60 m'
+    '_awd_ft_append 1 s'
     'unset _awd_start'
 )
 
@@ -29,7 +45,7 @@ PS1="${PRESET}"
 PS1+="${PBOLD}${PROMPT_FG_RED}"'${_awd_status:+${cerr} (${_awd_status}) }'
 
 # (Optionally) last command's run time.
-PS1+="${PROMPT_FG_BLACK}"'${_awd_t:+【┘】${_awd_t}s }'
+PS1+="${PROMPT_FG_BLACK}"'${_awd_ft:+【┘】${_awd_ft} }'
 
 # Datetime
 PS1+="${PROMPT_FG_GREEN}[ \d \t ] "
