2db49b423349423e341227fa8e31601f4e65d341
Al Dimond <ald@extrahop.com>
2025-10-29 14:12:10 -0700

    Document pyf

--- python/pyf.py
+++ python/pyf.py
@@ -8,20 +8,85 @@ import importlib
 import sys
 
 parser = argparse.ArgumentParser(
-    prog="pyf",
-    description="Runs a Python func with given args",
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description="""\
+Runs a Python func with given args and prints the result.
+
+The main point of this is to bring the enormous Python stdlib into shell
+scripts. The initial motivating example was that I wanted to urlencode a string
+in a shell script.
+
+This is slightly inspired by perl's ability to fit into shell scripts, but it's
+not made to be a filtering processor like perl is.
+""",
+    epilog="""\
+Examples:
+
+    - urlencode a string
+        $ %(prog)s urllib.parse.quote_plus 'a/b c'
+        a%%2Fb+c
+
+    - reverse a string (needs -c to iterate/concat the reversed() obj)
+        $ %(prog)s -c reversed aoeu
+        ueoa
+
+    - reverse a list of strings (needs -o to put args in a single list, -s to
+      iterate result)
+        $ %(prog)s -os reversed aoeu htns gcrl
+        gcrl htns aoeu
+
+    - ... as above, but with different output formatting
+        $ %(prog)s -oi reversed fghi cde ab
+        ['ab', 'cde', 'fghi']
+
+        $ %(prog)s -oc reversed fghi cde ab
+        abcdefghi
+
+    - Work with numbers (-l to evaluate args as literals, -n for newline
+      separation, -- to hide negative number from option parser)
+        $ %(prog)s -lon -- itertools.accumulate 1 5 20 -2 3
+        1
+        6
+        26
+        24
+        27
+
+    - ... Add -k to allow kwargs
+        $ %(prog)s -lkon -- itertools.accumulate 1 5 20 -2 3 initial=100
+        100
+        101
+        106
+        126
+        124
+        127
+
+    - Null-separate output with -0 to work with tools that want that
+        $ %(prog)s -0o reversed 'my hat' 'has' 'three corners' | xargs -0 printf '-> %%s\\n'
+        -> three corners
+        -> has
+        -> my hat
+
+    - Do floating-point math (needs -l to evaluate args as numbers)
+        $ %(prog)s -l math.pow 2.5 3.6
+        27.07597043574791o
+""",
 )
-parser.add_argument("--kwargs", "-k", action="store_true", help="Args with = are split on = as kwargs")
-parser.add_argument("--literal-args", "-l", action="store_true", help="Args and kwargs values are interpreted as Python literals (otherwise strings).")
-parser.add_argument("--one-list", "-o", action="store_true", help="Passes list of args as a single arg instead of starring them.")
-parser.add_argument("--iterate-result","-i", action="store_true", help="Force iteration of result via list().")
-parser.add_argument("--repr", "-r", action="store_true", help="Result is printed with repr() (otherwise as a string). This happens after --iterate-result but before --join-result.")
-joins = parser.add_mutually_exclusive_group()
+
+arg_opts = parser.add_argument_group("Argument handling")
+arg_opts.add_argument("--kwargs", "-k", action="store_true", help="Args with = are split on = as kwargs")
+arg_opts.add_argument("--literal-args", "-l", action="store_true", help="Args and kwargs values are interpreted as Python literals (otherwise strings).")
+arg_opts.add_argument("--one-list", "-o", action="store_true", help="Passes list of args as a single arg instead of starring them.")
+
+res_opts = parser.add_argument_group("Result handling")
+res_opts.add_argument("--iterate-result","-i", action="store_true", help="Force iteration of result via list(). Typically with --join-* options below this is not needed.")
+res_opts.add_argument("--repr", "-r", action="store_true", help="Result is formatted with repr() (otherwise with str()). This happens after --iterate-result but before --join-result.")
+joins = res_opts.add_mutually_exclusive_group()
 joins.add_argument("--join-result", "-j", metavar="STR", help="Join result with STR as separator.")
 joins.add_argument("--join-nulls", "-0", action="store_true", help="Join result with nulls and suppress trailing newline.")
 joins.add_argument("--join-space", "-s", action="store_true", help="Join result with spaces.")
 joins.add_argument("--join-empty", "-c", action="store_true", help="Join result with empty-string (concat into one string)")
 joins.add_argument("--join-newline", "-n", action="store_true", help="Join result with newlines")
+
 parser.add_argument("func_name", help="Qualified function name")
 parser.add_argument("func_args", nargs="*")
 
